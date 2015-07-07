a819b630dff604bc6c9033447134b4bfd5d57201
MarianYakubets <marian.yakubets@gmail.com>
2015-07-07 21:18:14 +0300

    Initial commit

---
+++ src/states/MainMenu.js
@@ -0,0 +1,24 @@
+
+SmathGame.MainMenu = function (game) {
+	this.playButton = null;
+};
+
+SmathGame.MainMenu.prototype = {
+
+	create: function () {
+		//	We've already preloaded our assets, so let's kick right into the Main Menu itself.
+		//	Here all we're doing is playing some music and adding a picture and button
+		//	Naturally I expect you to do something significantly better :)
+		this.add.sprite(0, 0, 'back');
+	},
+
+	update: function () {
+		//	Do some nice funky main menu effect here
+	},
+
+	startGame: function (pointer) {
+		//	And start the actual game
+		this.state.start('Game');
+	}
+
+};
